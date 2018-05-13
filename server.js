222155e37b4b849ceb4e1c42c6798deab39c0939
Kirill <moviegoodk@gmail.com>
2018-05-13 14:52:34 +0300

    Add regiter to mock server

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const server = jsonServer.create()
 const router = jsonServer.router('./database.json')
 const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
-//server.use(bodyParser.urlencoded({extended: true}))
-//server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
 server.use(jsonServer.defaults());
 
 const SECRET_KEY = '123456789'
@@ -30,18 +30,69 @@ function isAuthenticated({email, password}){
   return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
 
+function isExistUser(email, name) {
+    return userdb.users.findIndex(user => user.email === email && user.name === name) !== -1;
+}
+
+function createNewUser(email, password, name) {
+    let maxIndex = 0;
+    userdb.users.forEach(user => {
+        if (maxIndex < user.id) maxIndex = user.id;
+    });
+
+    return {
+        id: maxIndex + 1,
+        email,
+        name,
+        password
+    }
+}
+
 
 server.post('/auth/login', (req, res) => {
-  const {email, password} = req.body
+  const {email, password, name} = req.body
   if (isAuthenticated({email, password}) === false) {
     const status = 401
     const message = 'Incorrect email or password'
     res.status(status).json({status, message})
     return
   }
-  const access_token = createToken({email, password})
-  res.status(200).json({access_token})
+  const access_token = createToken({email, name, password})
+  res.status(200).json({
+      success: true,
+      access_token: access_token
+  })
 })
+
+server.post('/auth/registration', (req, res) => {
+    const {email, password, name} = req.body;
+
+    if (isExistUser(email, name)) {
+        const status = 400;
+        const message = 'User already exist';
+        res.status(status).json({
+            success: false,
+            message
+        });
+        return;
+    };
+
+    userdb.users.push(createNewUser(email, password, name));
+    json = JSON.stringify(userdb, null, 4);
+
+    fs.writeFile('./users.json', json, (err) => {
+        if (err) throw err;
+        console.log('User registered');
+    });
+
+    const access_token = createToken({email, name, password});
+    res.status(200).json({
+        success: true,
+        access_token: access_token
+    });
+})
+
+server.use('/api', router);
 
 server.use(/^(?!\/auth).*$/,  (req, res, next) => {
   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
@@ -60,8 +111,8 @@ server.use(/^(?!\/auth).*$/,  (req, res, next) => {
   }
 })
 
-server.use(router)
 
-server.listen(3000, () => {
+
+server.listen(3001, () => {
   console.log('Run Auth API Server')
 })
